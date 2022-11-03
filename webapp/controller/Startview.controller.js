8e11e2cd85eeadfef3d8dec89cda5cc40a53fc1f
Philipp Brandt <philipp.brandt@sap.com>
2022-11-03 12:38:58 +0000

    Added Truck Selection Dialog

--- webapp/controller/Startview.controller.js
+++ webapp/controller/Startview.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Fragment, MessageToast) {
         "use strict";
 
         return Controller.extend("finalproject.controller.Startview", {
@@ -12,6 +14,25 @@ sap.ui.define([
 
                 this.getView().byId("DatePicker").setDateValue(new Date());
 
+            },
+
+            onTruckPress: function (evt) {
+                if (!this.pDialog) {
+                    this.pDialog = this.loadFragment({
+                        name: "finalproject.view.TruckSelectionDialog"
+                    });
+                }
+                this.pDialog.then(function(oDialog) {
+                    oDialog.open();
+                });
+            },
+
+            onCloseDialog: function () {
+                this.byId("TruckSelection").close();
+            },
+
+            onCalcPress: function (evt) {
+                MessageToast.show("Calc Pressed");
             }
         });
     });
